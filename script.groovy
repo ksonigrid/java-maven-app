981ac3099e185e1c29dcbd6b6da8db01e87e4a38
Krishna soni <ksoni@griddynamics.com>
2024-09-13 12:55:37 +0530

    bug removed

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def BuildJar(){
 
 def BuildImage(){
     echo "building images"
-    withCredentials([usernamePassword(credentialsId: "dockerhub-credential", passwordVariable: 'USER', usernameVariable: 'PASS')]) {
+    withCredentials([usernamePassword(credentialsId: "dockerhub-credential", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh """
             docker build . -t ksonigrid/java-mavenrepo:2.0
             echo ${PASS} | docker login -u ${USER} --password-stdin
