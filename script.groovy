5ff4d32b16d690f7664aeb5453d920cafb93df80
Krishna soni <ksoni@griddynamics.com>
2024-09-25 11:57:20 +0530

    incrementing application version

--- script.groovy
+++ script.groovy
@@ -1,17 +1,25 @@
 // All environment variables available in Jenkinsfile are available in this file
-
+def increment_version() {
+    echo "incrementing the application version....."
+    sh '
+        mvn build-helper:parse-version versions:set \
+        -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
 def BuildJar(){
     echo "building Jar"
-    sh "mvn package"
+    sh "mvn clean package"
 }
 
 def BuildImage(){
     echo "building images"
     withCredentials([usernamePassword(credentialsId: "dockerhub-credential", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh """
-            docker build . -t ksonigrid/java-mavenrepo:2.0
+            docker build . -t ksonigrid/java-mavenrepo:${IMAGE_NAME}
             echo ${PASS} | docker login -u ${USER} --password-stdin
-            docker push ksonigrid/java-mavenrepo:2.0
+            docker push ksonigrid/java-mavenrepo:${IMAGE_NAME}
         """
     }
 }
